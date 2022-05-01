b10691c42056ea7a0a6ba30b9bf831ac97d7e2a4
harsha1222 <97946291+harsha1222@users.noreply.github.com>
2022-05-01 12:40:21 +0530

    Add files via upload

---
+++ Checkin-MicroService/src/test/java/CaseStudy/CheckinMicroService/CheckinMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package CaseStudy.CheckinMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ FlightBookingSystem-API_Gateway/src/main/java/apigateWay/TrainBookingSystemAPI_Gateway/TrainBookingSystemApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package apigateWay.TrainBookingSystemAPI_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBookingSystemApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainBookingSystemApiGatewayApplication.class, args);
+	}
+
+}
